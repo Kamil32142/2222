d765714e4e9e7e1abf6cb1596d6bfe622edf5063
Kamil32142 <160357117+Kamil32142@users.noreply.github.com>
2024-03-07 00:53:03 +0300

    django

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def item_list(request):
-    return HttpResponse("<h1>Список элементов</h1>")
+    return HttpResponse('<h1>Список элементов</h1>')
 
 
 def item_detail(request, pk):
-    return HttpResponse("<h1>Подробно элемент</h1>")
+    return HttpResponse('<h1>Подробно элемент</h1>')
