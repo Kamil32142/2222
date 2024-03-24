90a6132730bb5dff59003b8092b257257ba3fa58
Kamil32142 <160357117+Kamil32142@users.noreply.github.com>
2024-03-24 19:55:44 +0300

    django

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.contrib import admin
 from django.urls import include, path
-
 
 urlpatterns = [
     path('about/', include('about.urls')),
     path('catalog/', include('catalog.urls')),
     path('', include('homepage.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 
